6493ca1ec82043e619c3be063de3797baf848783
Marcin Magiera <marcin@magm.me>
2021-10-27 11:50:40 +0200

    Added Linear Background to App

--- Dev History/ContentView.swift
+++ Dev History/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let mainColorOne = Color(red: 172/255, green: 39/255, blue: 155/255)
+    let mainColorTwo = Color(red: 58/255, green: 20/255, blue: 144/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    let gradinetBg = LinearGradient(gradient: Gradient(colors: [Color.red, Color.purple]), startPoint: .top, endPoint: .bottom)
+    
     
     let white = Color(red: 1, green: 1, blue: 1)
     let whiteTheSame = Color(red: 1, green: 1, blue: 1)
@@ -18,7 +21,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            mainColor.ignoresSafeArea() 
+            gradinetBg.ignoresSafeArea()
         }
     }
 }
