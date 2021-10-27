f532577e92b79ed6aaaabf4c5614e65e770a4cd6
Marcin Magiera <marcin@magm.me>
2021-10-27 10:26:37 +0200

    Initial Commit

---
+++ Dev History/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dev History
+//
+//  Created by Magme on 27/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dev History/Dev_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_HistoryApp.swift
+//  Dev History
+//
+//  Created by Magme on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Dev_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
