343971208227cc661000088511d3fc1f9e33ac34
Marcin Magiera <marcin@magm.me>
2021-10-27 12:28:40 +0200

    Changed Linear Background Color

--- Dev History/ContentView.swift
+++ Dev History/ContentView.swift
@@ -12,7 +12,14 @@ struct ContentView: View {
     let mainColorOne = Color(red: 172/255, green: 39/255, blue: 155/255)
     let mainColorTwo = Color(red: 58/255, green: 20/255, blue: 144/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
-    let gradinetBg = LinearGradient(gradient: Gradient(colors: [Color.red, Color.purple]), startPoint: .top, endPoint: .bottom)
+    
+    // Gradient Color
+    let gradinetBg = LinearGradient(gradient: Gradient(colors: [
+        Color.purple,
+        Color.red
+    ]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing)
     
     
     let white = Color(red: 1, green: 1, blue: 1)
