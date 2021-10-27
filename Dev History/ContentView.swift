a7a21d83076e77b3dc781bf104f716676f7b152f
Marcin Magiera <marcin@magm.me>
2021-10-27 11:05:00 +0200

    Created project and added Two variables of main colors

--- Dev History/ContentView.swift
+++ Dev History/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
+    let white = Color(red: 1, green: 1, blue: 1)
+    let whiteTheSame = Color(red: 1, green: 1, blue: 1)
+    
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
